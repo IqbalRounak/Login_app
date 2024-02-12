2bcdbd0f9dc6b4c1de345ef74586558f049de629
IqbalRounak <rounakiqbal6@gmail.com>
2024-02-12 14:31:13 +0530

    login commit

---
+++ src/main/java/p1/Login.java
@@ -0,0 +1,52 @@
+package p1;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public Login() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/logindb_demo","root","sa123");
+			Statement stmnt = con.createStatement();
+			ResultSet result = stmnt.executeQuery("select * from login where email='"+email+"' and password='"+password+"'");
+			
+			if(result.next()) {
+				System.out.println("welcome");
+			}else{
+				System.out.println("wrong email or pass");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
